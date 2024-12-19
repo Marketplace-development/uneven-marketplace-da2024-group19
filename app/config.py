4575a7ea43b894afe62b16287ee99d3b13492146
elisedeleu <elise.deleu09@gmail.com>
2024-12-19 21:34:06 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 class Config: 
-    SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.username:password@localhost:port/dbname'
+    SECRET_KEY = '2QRu2Qh8BmeF4zuS'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.sfpznediurpyqgbdkphw:2QRu2Qh8BmeF4zuS@aws-0-eu-central-1.pooler.supabase.com:6543/postgres?gssencmode=disable'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
