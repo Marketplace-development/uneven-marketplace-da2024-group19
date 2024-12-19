4f3daf0d09b60279e45061fcf1b9d31347b6f0cd
elisedeleu <elise.deleu09@gmail.com>
2024-12-19 21:59:01 +0100

    Delete app/config.py

--- app/config.py
+++
@@ -1,4 +0,0 @@
-class Config: 
-    SECRET_KEY = '2QRu2Qh8BmeF4zuS'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.sfpznediurpyqgbdkphw:2QRu2Qh8BmeF4zuS@aws-0-eu-central-1.pooler.supabase.com:6543/postgres?gssencmode=disable'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
